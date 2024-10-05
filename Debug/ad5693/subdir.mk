34c389ad2cc9c41736a5870f688de9f7f44f9148
rahul <rahul@cdspace.in>
2024-10-05 21:19:15 +0530

    add basic function

---
+++ Debug/ad5693/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ad5693/ad5693.c 
+
+OBJS += \
+./ad5693/ad5693.o 
+
+C_DEPS += \
+./ad5693/ad5693.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ad5693/%.o ad5693/%.su ad5693/%.cyclo: ../ad5693/%.c ad5693/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"E:/STM32CubeIDE/ad5693_lib/ad5693" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-ad5693
+
+clean-ad5693:
+	-$(RM) ./ad5693/ad5693.cyclo ./ad5693/ad5693.d ./ad5693/ad5693.o ./ad5693/ad5693.su
+
+.PHONY: clean-ad5693
+
